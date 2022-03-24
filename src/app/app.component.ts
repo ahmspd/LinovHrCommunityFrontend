a19fd2456140aa0e24471c0ebe821c242f9a4dc3
Hammid Muammar <spinxclopz21@gmail.com>
2022-03-24 13:41:28 +0700

    init prime ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,17 @@
-import { Component, OnInit, Inject, Renderer2, ElementRef, ViewChild } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
-import 'rxjs/add/operator/filter';
-import { DOCUMENT } from '@angular/common';
-import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-
+import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-    private _router: Subscription;
-    @ViewChild(NavbarComponent) navbar: NavbarComponent;
+export class AppComponent {
 
-    constructor( private renderer : Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+    menuMode = 'static';
+
+    constructor(private primengConfig: PrimeNGConfig) { }
+
     ngOnInit() {
-        var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
-        this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
-            if (window.outerWidth > 991) {
-                window.document.children[0].scrollTop = 0;
-            }else{
-                window.document.activeElement.scrollTop = 0;
-            }
-            this.navbar.sidebarClose();
-
-            this.renderer.listen('window', 'scroll', (event) => {
-                const number = window.scrollY;
-                var _location = this.location.path();
-                _location = _location.split('/')[2];
-
-                if (number > 150 || window.pageYOffset > 150) {
-                    navbar.classList.remove('navbar-transparent');
-                } else if (_location !== 'login' && this.location.path() !== '/nucleoicons') {
-                    // remove logic
-                    navbar.classList.add('navbar-transparent');
-                }
-            });
-        });
+        this.primengConfig.ripple = true;
+        document.documentElement.style.fontSize = '14px';
     }
 }
