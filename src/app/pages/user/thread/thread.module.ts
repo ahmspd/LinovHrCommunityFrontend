18315ab2ee537d2b97be42341aff8357ebec4bf9
anduajulian <andua.julian@gmail.com>
2022-03-25 17:55:03 +0700

    +fix homapage | + thread-list

--- src/app/pages/user/thread/thread.module.ts
+++ src/app/pages/user/thread/thread.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { ButtonModule } from "primeng/button";
+import { TagModule } from "primeng/tag";
 import { ThreadListComponent } from "./thread-list/thread-list.component";
 import { ThreadRouter } from "./thread.router";
 
@@ -12,7 +13,8 @@ import { ThreadRouter } from "./thread.router";
     imports: [
         ThreadRouter,
         RouterModule,
-        ButtonModule
+        ButtonModule,
+        TagModule
     ]
 })
 export class ThreadModule{}
